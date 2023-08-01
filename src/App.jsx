73b773aadaa0794aa63625efbc92715389556292
Nugget-Meister <tremainecamacho@gmail.com>
2023-07-31 22:01:50 -0400

    Lab done

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,68 @@
 import Footer from "./Footer";
 import Header from "./Header";
+import menuItems from "./data";
+import { useState } from "react";
 
 function App() {
+  const [order, setOrder] = useState([])
+  const [total, setTotal] = useState(0)
+
+
+  function addOrder(item) {
+    const newOrder = {
+      name: item.name,
+      price: item.price,
+      id: order.length-1
+    }
+
+    setOrder([...order, newOrder])
+    setTotal(total+item.price)
+  }
+
+  function removeItem({id, price}){
+    const filteredOrders = order.filter((item) => item.id !== id)
+    setOrder(filteredOrders)
+    setTotal(total-price)
+  }
+
   return (
     <div className="App">
       <Header />
       <main>
         <aside>
-          <table></table>
+          <table>
+            <tbody>
+            {menuItems.map((item) => {
+              return (
+                <tr key={item.id} onClick={() => addOrder(item)}>
+                  <td>{item.image}</td>
+                  <td className="item-name">
+                    <span>{item.name}</span><br/>
+                    <span>{"🌶️".repeat(item.spiceLevel)}</span>
+                  </td>
+                  <td>${item.price}</td>
+                </tr>
+              )  
+            }
+            )}
+            </tbody>
+          </table>
         </aside>
         <section>
           <div>
             <h2>Current Order</h2>
-            <ul></ul>
-            <h4>Total:</h4>
+            <ul>
+              {order.map((item) => {
+                return (
+                  <li key={item.id}>
+                    <span onClick={() => removeItem(item)}>❌</span>
+                    <span>{item.name}</span>
+                    <span>{item.price}</span>
+                  </li>
+                )
+              })}
+            </ul>
+            <h4>Total: {total}</h4>
             <div>
               <button>Tidy order</button>
               <button>Close order</button>
