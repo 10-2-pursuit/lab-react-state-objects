cf217ad9f0759ef21e9fbf905a7e2714e8377171
winder joel marte <winderjoelmarte@winders-Air.fios-router.home>
2023-08-07 16:43:17 -0400

    mi hermano me ayudo

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,75 @@
 import Footer from "./Footer";
 import Header from "./Header";
+import List from "./components/List";
+import data from "./data"
+import React,{useState} from "react";
+
 
 function App() {
+  const [currentOrder,setCurrentOrder] = useState([])
+  const [totalCost, setTotalCost] = useState(0);
+
+  const addToOrder = (item) => {
+    setCurrentOrder([...currentOrder, item]);
+    setTotalCost((prevTotal) => prevTotal + item.price);
+  };
+
+  const removeFromOrder = (index) => {
+    const removedItem = currentOrder[index];
+    setCurrentOrder(currentOrder.filter((_, i) => i !== index));
+    setTotalCost((prevTotal) => prevTotal - removedItem.price);
+  };
+
+  const tidyOrder = () => {
+    const tidiedOrder = currentOrder.reduce((acc, item) => {
+      const existingItem = acc.find((el) => el.id === item.id);
+      existingItem
+        ? (existingItem.quantity += 1)
+        : acc.push({ ...item, quantity: 1 });
+      return acc;
+    }, []);
+    setCurrentOrder(tidiedOrder);
+    setTotalCost(tidiedOrder.reduce((total, item) => total + item.price * item.quantity, 0));
+  };
+
+  const closeOrder = () => {
+    setCurrentOrder([]);
+    setTotalCost(0);
+  };
   return (
     <div className="App">
-      <Header />
-      <main>
-        <aside>
-          <table></table>
-        </aside>
-        <section>
+    <Header />
+    <main>
+      <aside>
+        <table>
+          <tbody>
+            {data.map((item) => (
+              <List key={item.id} item={item} onAddToOrder={addToOrder} /> 
+            ))}
+          </tbody>
+        </table>
+      </aside>
+      <section>
+        <div>
+          <h2>Current Order</h2>
+          <ul>
+            {currentOrder.map((item, index) => (
+              <li key={item.id}>
+                {item.name} - ${item.price} x {item.quantity}{' '}
+                <button onClick={() => removeFromOrder(index)}>❌</button>
+              </li>
+            ))}
+          </ul>
+          <h4>Total: ${totalCost}</h4>
           <div>
-            <h2>Current Order</h2>
-            <ul></ul>
-            <h4>Total:</h4>
-            <div>
-              <button>Tidy order</button>
-              <button>Close order</button>
-            </div>
+            <button onClick={tidyOrder}>Tidy order</button>
+            <button onClick={closeOrder}>Close order</button>
           </div>
-        </section>
-      </main>
-      <Footer />
-    </div>
+        </div>
+      </section>
+    </main>
+    <Footer />
+  </div>
   );
 }
 
